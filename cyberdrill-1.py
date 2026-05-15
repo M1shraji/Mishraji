e70dd83532c1fdaac6ca2e7c29de773a6ed0db21
M1shraji <hrishikeshmishra2007@gmail.com>
2026-05-15 23:05:17 +0530

    Add files via upload

---
+++ cyberdrill-1.py
@@ -0,0 +1,10 @@
+hacker_name = "Mishraji"
+target_ip = "10. 10. 10. 5"
+port = 22
+is_root = False
+lpa_goal = 25.0
+print(f"Operator: {hacker_name}")
+print(f"Target: {target_ip}:{port}")
+print(f"Root access: {is_root}")
+print(f"Mission: {lpa_goal} LPA")
+print(type(is_root))
---
+++ day_counter.py
@@ -0,0 +1,7 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========================== RESTART: D:/day_conter.py ==========================
+Name: Hrishi
+ Days you coded so far: 0
+Day 1: Hrishi coding. Dell 8GB = no excuse. 25 LPA coming.
